85af11d4c79d5f54b5a2a5ab1ee63bd66ce9f5a1
H.M Atahar Nur <atahar.nur@shadhinlab.com>
2022-02-25 23:57:20 +0600

    updated lambda functions

--- src/log_in.py
+++ src/log_in.py
@@ -4,33 +4,43 @@ import boto3
 
 client = boto3.client('cognito-idp')
 
-ERROR = 0
-SUCCESS = 1
-
-def log_in(username, password):
-    try:
-        response = client.sign_up(
-            ClientId=os.environ.get('COGNITO_USER_CLIENT_ID'),
-            Username=username,
-            Password=password)
-        print(response)
-    except client.exceptions.InvalidPasswordException as e:
-        return ERROR
-    except client.exceptions.NotAuthorizedException as e:
-        return ERROR
-    except Exception as e:
-        print(e)
-        return ERROR
-    return SUCCESS
+def authenticate(username, password):
+    response = client.initiate_auth(
+        ClientId=os.environ.get('COGNITO_USER_CLIENT_ID'),
+        AuthFlow = 'USER_PASSWORD_AUTH',
+        Username=username,
+        Password=password)
+    print(response)
+    access_token = response['AuthenticationResult']['AccessToken']
+    refresh_token = response['AuthenticationResult']['RefreshToken']
+    print('AccessToken: ',access_token)
+    print('RefreshToken: ',refresh_token)
+    return access_token, refresh_token
 
 def lambda_handler(event, context):
-
     print(event)
     body = json.loads(event['body'])
     username = body["username"]
     password = body["password"]
-    logged_in = log_in(username, password)
-    if logged_in == ERROR:
-        return {'status': 'fail', 'msg': 'failed to log in'}
-    if logged_in == SUCCESS:
-        return {'status': 'success', 'msg': 'log in successful'}
+    try:
+        authenticated = authenticate(username, password)
+        print(authenticated)
+        return{
+            'statusCode': 200,
+            'message': 'log in successful.',
+        }
+    except client.exceptions.UserNotConfirmedException as e:
+        return {
+            'statusCode': 400,
+            'message': 'User not confirmed yet. Please check email for confirmation code'
+    }
+    except client.exceptions.InvalidPasswordException as e:
+        return {
+            'statusCode': 400,
+            'message': 'Password is invalid. Please check again.'
+        }
+    except Exception as e:
+        print(e)
+        return{
+            'message': 'Unknown Error'
+        }
--- src/sign_up.py
+++ src/sign_up.py
@@ -4,34 +4,44 @@ import json
 
 client = boto3.client('cognito-idp')
 
-ERROR = 0
-SUCCESS = 1
-USER_EXISTS = 2
-
 def sign_up(username, password):
-    try:
-        response = client.sign_up(
-            ClientId=os.environ.get('COGNITO_USER_CLIENT_ID'),
-            Username=username,
-            Password=password)
-        print(response)
-    except client.exceptions.UsernameExistsException as e:
-        return USER_EXISTS
-    except client.exceptions.InvalidPasswordException as e:
-        return ERROR
-    except Exception as e:
-        print(e)
-        return ERROR
-    return SUCCESS
+    response = client.sign_up(
+        ClientId=os.environ.get('COGNITO_USER_CLIENT_ID'),
+        Username=username,
+        Password=password,
+        UserAttributes=[
+        {
+            'Name': 'email',
+            'Value': username
+        },
+    ],        
+    )
+    print(response)
+    return response
 
 def lambda_handler(event, context):
     print(event)
     body = json.loads(event['body'])
     username = body["username"]
     password = body["password"]
-    signed_up = sign_up(username, password)
-    if signed_up == ERROR:
-        return {'status': 'fail', 'msg': 'failed to sign up'}
-    if signed_up == SUCCESS:
-        return {'status': 'success', 'msg': 'sign up successful'}
-
+    try:
+        signed_up = sign_up(username, password)
+        return{
+            'statusCode': 200,
+            'message': 'sign up successful. To confirm please check email for confirmation code.',
+        }
+    except client.exceptions.UsernameExistsException as e:
+        return {
+            'statusCode': 400,
+            'message': 'Username already exists'
+    }
+    except client.exceptions.InvalidPasswordException as e:
+        return {
+            'statusCode': 400,
+            'message': 'Password is invalid. Please check again.'
+        }
+    except Exception as e:
+        print(e)
+        return{
+            'message': 'Unknown Error'
+        }
